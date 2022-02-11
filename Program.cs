2e3a64e5fe4274b65e0d7420f052108645cd8b24
Maria Eganyan <maria.eganyan77@gmail.com>
2022-02-11 12:01:18 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalculateProjeck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
